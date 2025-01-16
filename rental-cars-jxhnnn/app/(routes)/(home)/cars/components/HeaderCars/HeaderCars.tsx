6b4adc14b0107513241179545761a483325022ca
JohnAlex22 <johnlaex2207@gmail.com>
2025-01-15 22:42:10 -0500

    Rental Cars JxhNNN

---
+++ rental-cars-jxhnnn/app/(routes)/(home)/cars/components/HeaderCars/HeaderCars.tsx
@@ -0,0 +1,9 @@
+export function HeaderCars() {
+  return (
+    <div className="my-8">
+      <h1 className="text-3xl font-bold">
+        Elige el vehículo que desees usar
+      </h1>
+    </div>
+  );
+}
